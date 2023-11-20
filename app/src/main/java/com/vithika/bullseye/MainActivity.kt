20eb9d7aff0f721fea9c49bb20c5bb78136079dd
vithika <vgvithikagupta1@gmail.com>
2023-11-20 11:29:34 +0530

    Finalised Game with Icon and SplashScreen

--- app/src/main/java/com/vithika/bullseye/MainActivity.kt
+++ app/src/main/java/com/vithika/bullseye/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -17,6 +18,7 @@ import com.vithika.bullseye.ui.theme.BullsEyeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContent {
             BullsEyeTheme {
